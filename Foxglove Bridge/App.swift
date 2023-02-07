52596494384e0b156be0d9dc140fb293f3c4bded
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-02-07 13:21:01 -0700

    initial server working

---
+++ Foxglove Bridge/App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Foxglove_BridgeApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Foxglove Bridge/ContentView.swift
@@ -0,0 +1,119 @@
+import SwiftUI
+import Network
+import Darwin
+
+// https://stackoverflow.com/a/73853838/23649
+func getIPAddress() -> String? {
+    var address : String?
+
+    // Get list of all interfaces on the local machine:
+    var ifaddr : UnsafeMutablePointer<ifaddrs>?
+    guard getifaddrs(&ifaddr) == 0 else { return nil }
+    guard let firstAddr = ifaddr else { return nil }
+
+    // For each interface ...
+    for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+        let interface = ifptr.pointee
+
+        // Check for IPv4 or IPv6 interface:
+        let addrFamily = interface.ifa_addr.pointee.sa_family
+        if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+
+            // Check interface name:
+            // wifi = ["en0"]
+            // wired = ["en2", "en3", "en4"]
+            // cellular = ["pdp_ip0","pdp_ip1","pdp_ip2","pdp_ip3"]
+
+            let name = String(cString: interface.ifa_name)
+            if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+
+                // Convert interface address to a human readable string:
+                var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                            &hostname, socklen_t(hostname.count),
+                            nil, socklen_t(0), NI_NUMERICHOST)
+                address = String(cString: hostname)
+            }
+        }
+    }
+    freeifaddrs(ifaddr)
+
+    return address
+}
+
+class Server: ObservableObject {
+  let queue = DispatchQueue(label: "ServerQueue")
+
+  let address = getIPAddress() ?? "<no address>"
+
+  @MainActor @Published var port: NWEndpoint.Port?
+
+  init() {
+    do {
+      let params = NWParameters(tls: nil)
+//      params.includePeerToPeer = true
+      print(params.defaultProtocolStack.applicationProtocols)
+      params.defaultProtocolStack.applicationProtocols.append(NWProtocolWebSocket.Options())
+
+      let listener = try NWListener(using: params)
+      listener.stateUpdateHandler = { newState in
+        if let port = listener.port {
+          print("Listening on \(self.address):\(port)")
+        }
+        Task { @MainActor in
+          self.port = listener.port
+        }
+      }
+      listener.newConnectionHandler = { [weak self] newConnection in
+        guard let self else { return }
+        print("New conn: \(newConnection)")
+
+        func receive() {
+          newConnection.receiveMessage { data, context, isComplete, error in
+            if let data, let context {
+              print("Got message: \(data), \(isComplete), \(error)")
+
+              receive()
+            }
+          }
+
+          newConnection.stateUpdateHandler = { state in
+            print("connection state \(state)")
+          }
+        }
+        receive()
+
+
+        newConnection.start(queue: self.queue)
+      }
+      listener.start(queue: queue)
+
+    } catch let error {
+      print("Error \(error)")
+    }
+  }
+}
+
+@MainActor
+struct ContentView: View {
+  @StateObject var server = Server()
+
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
+      if let port = server.port {
+        Text("Listening on \(server.address):\(port.debugDescription)")
+      }
+    }
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
